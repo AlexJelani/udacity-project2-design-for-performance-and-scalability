abbffbdcc899926a4952a04f36cd48d55eb0faeb
AlexJelani <jelanialexander82@gmail.com>
2024-12-05 17:37:39 +0900

    feat: add S3 lifecycle policy for media storage optimization"

--- exercise1/main.tf
+++ exercise1/main.tf
@@ -5,13 +5,35 @@ provider "aws" {
 resource "aws_s3_bucket" "exercise_bucket" {
   bucket = "udacity-exercise-bucket1-c527000"
   force_destroy = true
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
+  bucket = aws_s3_bucket.exercise_bucket.id
+
+  rule {
+    id = "media_lifecycle"
+    status = "Enabled"
+
+    filter {}
+
+    # After 30 days, move to IA since views decrease
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    # After 90 days, move to Glacier for cold storage
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+
+    # Delete after 180 days as per requirement
+    expiration {
+      days = 180
+    }
+  }
+}
 
 resource "random_id" "bucket_suffix" {
   byte_length = 4
